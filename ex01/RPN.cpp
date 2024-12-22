3391e7db5fd3826c75f657548d162d00091be086
elsa2491 <elsa.touma@gmail.com>
2024-12-22 01:06:53 +0100

    Split code into 2 files

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -5,9 +5,63 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/05 16:19:49 by eltouma           #+#    #+#             */
-/*   Updated: 2024/12/05 16:20:06 by eltouma          ###   ########.fr       */
+/*   Created: 2024/12/05 16:03:22 by eltouma           #+#    #+#             */
+/*   Updated: 2024/12/22 01:05:55 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
+
+double	doOp(double first, double second, char op)
+{
+	switch (op)
+	{
+		case '+':
+			return (first + second);
+		case '-':
+			return (second - first);
+		case '*':
+			return (first * second);
+		case '/':
+			if (first == 0)
+				throw std::invalid_argument("ZeroDivisionError (divided by 0)");
+			return (second / first);
+	}
+	return (0);
+}
+
+int	checkInput(char *s, std::list<double> &lst)
+{
+	std::string	str;
+	size_t	size;
+	long	nb;
+	double	result;
+	double	first;
+	double	second;
+	char	*endptr;
+
+	str = s;
+	size = str.length();
+	if ((str[0] == '+' || str[0] == '-' || str[0] == '/' || str[0] == '*') && size == 1)
+	{
+		if (lst.size() < 2)
+			return (1);
+		first = lst.back();
+		lst.pop_back();
+		second = lst.back();
+		lst.pop_back();
+		result  = doOp(first, second, str[0]);
+		lst.push_back(result);
+		return (0);
+	}
+	errno = 0;
+	nb = strtol(str.c_str(), &endptr, 10);
+	if (endptr == str.c_str() || *endptr != '\0')
+		return (std::cerr << "Error\nBad input" << std::endl, 1);
+	if (errno == ERANGE || nb < INT_MIN)
+		return (std::cerr << "Error\nOut of range" << std::endl, 1);
+	if (nb > 9)
+		return (std::cerr << "Error\nGreater than or equal to 10" << std::endl, 1);
+	lst.push_back(nb);
+	return (0);
+}
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,8 +6,18 @@
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 16:20:15 by eltouma           #+#    #+#             */
-/*   Updated: 2024/12/06 13:50:06 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/12/22 01:05:31 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <limits.h>
+#include <cerrno>
+#include <list>
+
+double	doOp(double first, double second, char op);
+int	checkInput(char *s, std::list<double> &lst);
